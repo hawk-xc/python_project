64e85f94fe6ebc4f330f3ad0f73dd07723d8833e
wahyu_bsi_acc <12221171@bsi.ac.id>
2022-10-25 15:22:24 +0700

    update data

--- file.belajar/WAHYU_TRI_CAHYONO_12221171_KARTUGANDENG.py
+++ file.belajar/WAHYU_TRI_CAHYONO_12221171_KARTUGANDENG.py
@@ -1,56 +1,36 @@
-M = (
-    'ratu',
-    'dimpil',
-    'ciwir',
-    'gundul',
-    'babi',
-    'tengkrang'
-)
-H = (
-    'petik',
-    'plompong',
-    'gunung',
-    'cawang',
-    'kantong',
-    'kerok'
-)
-nilai = ''
-warna = ''
+    M = (
+        'Zero',
+        'ratu',
+        'dimpil',
+        'ciwir',
+        'gundul',
+        'babi',
+        'tengkrang'
+    )
+    H = (
+        'Zero',
+        'petik',
+        'plompong',
+        'gunung',
+        'cawang',
+        'kantong',
+        'kerok'
+    )
+    nilai = ''
+    warna = ''
 
-while warna == 'quit':
-    nilai = int(input('masukkan nilai kartu [1-6] '))
-    warna = input('masukkan kode warna [H/h, M/m] ')
-    if nilai in range(1, 7):
-        if warna == "h" or warna == "H":
-            if nilai == 1:
-                print (H[0])
-            elif nilai == 2:
-                print (H[1])
-            elif nilai == 3:
-                print (H[2])
-            elif nilai == 4:
-                print (H[3])
-            elif nilai == 5:
-                print (H[4])
-            elif nilai == 6:
-                print (H[5])
-    
-        elif warna == "m" or warna == "M":
-            if nilai == 1:
-                print (M[0])
-            elif nilai == 2:
-                print (M[1])
-            elif nilai == 3:
-                print (M[2])
-            elif nilai == 4:
-                print (M[3])
-            elif nilai == 5:
-                print (M[4])
-            elif nilai == 6:
-                print (M[5])
-    
+    while warna != 'quit':
+        nilai = int(input('masukkan nilai kartu [1-6] '))
+        warna = input('masukkan kode warna [H/h, M/m] ')
+        if nilai in range(1, 7):
+            if warna == "h" or warna == "H":
+                print(H[nilai])
+        
+            elif warna == "m" or warna == "M":
+                print(M[nilai])
+        
+            else:
+                print ('kode warna yang anda inputkan salah, kode warna yang terdaftar ialah [H/h, M/m]')
+        
         else:
-            print ('kode warna yang anda inputkan salah, kode warna yang terdaftar ialah [H/h, M/m]')
-    
-    else:
-        print ('angka', nilai, 'yang anda input kan salah, rentang nilai valid adalah [1-6]')
+            print ('angka', nilai, 'yang anda input kan salah, rentang nilai valid adalah [1-6]')
